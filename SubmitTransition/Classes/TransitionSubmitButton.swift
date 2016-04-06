7e7dbd3457d8ad2dcdbc156a7ca551d81d786a75
Weilson Wonder <weilson@live.com>
2016-04-06 16:44:36 +0800

    -

--- SubmitTransition/Classes/TransitionSubmitButton.swift
+++ SubmitTransition/Classes/TransitionSubmitButton.swift
@@ -22,6 +22,8 @@ public class TKTransitionSubmitButton : UIButton, UIViewControllerTransitioningD
     let shrinkCurve = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
     let expandCurve = CAMediaTimingFunction(controlPoints: 0.95, 0.02, 1, 0.05)
     let shrinkDuration: CFTimeInterval  = 0.1
+    var originalWidth: CGFloat = 0
+    var originalCornerRadius: CGFloat = 0
     @IBInspectable public var normalCornerRadius:CGFloat? = 0.0{
         didSet {
             self.layer.cornerRadius = normalCornerRadius!
@@ -41,14 +43,16 @@ public class TKTransitionSubmitButton : UIButton, UIViewControllerTransitioningD
     }
 
     func setup() {
+        self.originalWidth = frame.width
         self.clipsToBounds = true
         spiner.spinnerColor = spinnerColor
     }
-
-    public func startLoadingAnimation() {
+    
+    public func startLoadingAnimation(completion: (Void -> Void)?) {
         self.cachedTitle = titleForState(.Normal)
         self.setTitle("", forState: .Normal)
-        UIView.animateWithDuration(0.1, animations: { () -> Void in
+        self.originalCornerRadius = self.layer.cornerRadius
+        UIView.animateWithDuration(0.1, animations: {
             self.layer.cornerRadius = self.frame.height / 2
             }) { (done) -> Void in
                 self.shrink()
@@ -56,21 +60,30 @@ public class TKTransitionSubmitButton : UIButton, UIViewControllerTransitioningD
                     
                     self.spiner.animation()
                 }
+                completion?()
         }
-        
+    }
+    
+    public func stopLoadingAnimation(completion: (Void -> Void)?) {
+        setTitle(self.cachedTitle, forState: UIControlState.Normal)
+        UIView.animateWithDuration(0.1, animations: {
+            self.layer.cornerRadius = self.originalCornerRadius
+            }) { (done) -> Void in
+                self.revertShrink()
+                self.spiner.stopAnimation()
+                completion?()
+        }
     }
 
-    public func startFinishAnimation(delay: NSTimeInterval, completion:(()->())?) {
-        NSTimer.schedule(delay: delay) { timer in
-            self.didEndFinishAnimation = completion
-            self.expand()
-            self.spiner.stopAnimation()
-        }
+    public func startFinishAnimation(completion: (Void -> Void)?) {
+        self.didEndFinishAnimation = completion
+        self.expand()
+        self.spiner.stopAnimation()
     }
 
     public func animate(duration: NSTimeInterval, completion:(()->())?) {
-        startLoadingAnimation()
-        startFinishAnimation(duration, completion: completion)
+        startLoadingAnimation(nil)
+        startFinishAnimation(completion)
     }
 
     public func setOriginalState() {
@@ -78,7 +91,7 @@ public class TKTransitionSubmitButton : UIButton, UIViewControllerTransitioningD
         self.spiner.stopAnimation()
     }
     
-    public override func animationDidStop(anim: CAAnimation!, finished flag: Bool) {
+    public override func animationDidStop(anim: CAAnimation, finished flag: Bool) {
         let a = anim as! CABasicAnimation
         if a.keyPath == "transform.scale" {
             didEndFinishAnimation?()
@@ -93,6 +106,17 @@ public class TKTransitionSubmitButton : UIButton, UIViewControllerTransitioningD
         self.layer.removeAllAnimations()
         self.setTitle(self.cachedTitle, forState: .Normal)
         self.spiner.stopAnimation()
+    }
+    
+    func revertShrink() {
+        let revertShrinkAnim = CABasicAnimation(keyPath: "bounds.size.width")
+        revertShrinkAnim.fromValue = frame.height
+        revertShrinkAnim.toValue = self.originalWidth
+        revertShrinkAnim.duration = shrinkDuration
+        revertShrinkAnim.timingFunction = shrinkCurve
+        revertShrinkAnim.fillMode = kCAFillModeForwards
+        revertShrinkAnim.removedOnCompletion = false
+        layer.addAnimation(revertShrinkAnim, forKey: revertShrinkAnim.keyPath)
     }
     
     func shrink() {
